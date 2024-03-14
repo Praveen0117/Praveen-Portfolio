f80dec54ef2f4e069e020b20d3c61ed65e1e821d
Praveen0117 <polakipraveenkumar9@gmail.com>
2024-03-14 19:30:34 +0530

    commit message

---
+++ react.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const portpolio = () => {
+    return(<h1>Praveen</h1>)
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<portpolio />)
